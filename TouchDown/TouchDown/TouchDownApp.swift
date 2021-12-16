c10890fe3dee69b70cc1f9dd33f2a07e6754dae9
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-16 18:36:55 +0300

    First Commit

---
+++ TouchDown/TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by mehmet karanlık on 16.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
