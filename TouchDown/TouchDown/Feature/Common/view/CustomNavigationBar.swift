352f686c7109db477d00bcc0a3f40b4aa1b6a98d
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-22 01:16:08 +0300

    A new start

--- TouchDown/TouchDown/Feature/Common/view/CustomNavigationBar.swift
+++ TouchDown/TouchDown/Feature/Common/view/CustomNavigationBar.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct CustomNavigationBar: View {
+    // MARK:  properties
+    @State private var isAnimating : Bool = false
+    
+    // MARK:  body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button(action: {}) {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+            Spacer()
+            LogoView()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(x: 0, y: isAnimating ? 0 : -25)
+                .onAppear {
+                    withAnimation(.easeOut(duration: 1)) {
+                        isAnimating.toggle()
+                    }
+                }
+            Spacer()
+            ZStack {
+                Button(action: {}) {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                }
+            Circle()
+                    .fill(Color.red)
+                    .frame(width: 14, height: 14, alignment: .center)
+                    .offset(x: 13, y: -10)
+            }
+        }
     }
 }
 
+
+
+
+// MARK:  preview
 struct CustomNavigationBar_Previews: PreviewProvider {
     static var previews: some View {
         CustomNavigationBar()
---
+++ TouchDown/TouchDown/Feature/Common/view/LogoView.swift
@@ -0,0 +1,38 @@
+//
+//  LogoView.swift
+//  TouchDown
+//
+//  Created by mehmet karanlık on 22.12.2021.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack(spacing:4) {
+            Text("touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            
+            Text("down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
